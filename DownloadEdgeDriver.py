a6306490ff65942b4c05412efbba93a2a66b0112
Dmitrijs Livinskis <dlivinskis@gmail.com>
2022-09-04 10:58:32 +0300

    Added Unzippig and new Comments

--- DownloadEdgeDriver.py
+++ DownloadEdgeDriver.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 """
 Created by Dmitrijs Livinskis; 03.09.2022
+
 Supporting material taken from:
     https://www.youtube.com/watch?v=HDEvWfSk2So "How to download files from Web using Python and requests library"
-    https://pynative.com/python-list-files-in-a-directory/
+    https://pynative.com/python-list-files-in-a-directory/ 
+    https://stackoverflow.com/questions/3451111/unzipping-files-in-python "Unarchiving files"
     
 """
 
 #libraries
-import requests
-import os
+import requests, os, zipfile
+
 #/libraries
 
 #Get Current Edge Version;There is a folder that is named exactly as Version of Edge. Name example "105.0.1343.25"
@@ -30,9 +32,18 @@ with open(filename, 'wb') as f:
             f.write(chunk)
 #/Download the File
 
+#Unzip archive
+with zipfile.ZipFile("W:\Coding\PythonProjects\DownloadFile\edgedriver_win64.zip", 'r') as zip_ref:
+    zip_ref.extractall("W:\Coding\PythonProjects\DownloadFile")
+#/Unzip archive   
 
+#Delete Old Edgedriver
+#/Delete Old Edgedriver
 
+#Rename msedgedrive to edgedriver
+#/Rename msedgedrive to edgedriver
 
-
+#Move new edgedriver to folder
+#/Move new edgedriver to folder
 
 
